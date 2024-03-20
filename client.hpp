1f290d94ba8c3f0063f10ed9917bf5ecec793b8a
Ahmed Ennaki <aennaki@e2r2p2.1337.ma>
2024-03-20 00:49:37 +0000

    server

--- client.hpp
+++ client.hpp
@@ -5,17 +5,20 @@ class Client {
 		std::string nickName;
 		std::string userName;
 		std::string realName;
+		bool auth;
 	public:
-		Client() {};
+		Client() : auth(0) {};
 		int getFd() {return fd;}
 		std::string getIpadd() {return ipAdd;}
 		std::string getNickname() {return nickName;}
 		std::string getUsername() {return userName;}
 		std::string getRealname() {return realName;}
+		bool getAuth() {return auth;}
 
 		void setFd(int fd) {this->fd = fd;}
 		void setIpAdd(std::string ipadd) {ipAdd = ipadd;}
 		void setNickname(std::string nickName) {this->nickName = nickName;}
 		void setUsername(std::string userName) {this->userName = userName;}
 		void setRealname(std::string realName) {this->realName = realName;}
+		void setAuth(bool auth) {this->auth = auth;}
 };
